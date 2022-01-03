5101239de65300fac5bcb6b45b3b009e0fa5cba5
Sergey Scherbakov <sherbakov_s_a@mail.ru>
2022-01-03 11:26:44 +0300

    Reformat code

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,7 @@
 public class CreditPaymentService {
     public long calculate(int credit, int period, double interestRate) {
         double index = interestRate / 12 / 100;
-        long payment = (long) (credit * ((index * Math.pow(1+index, period))/(Math.pow(1+index, period) - 1)));
+        long payment = (long) (credit * ((index * Math.pow(1 + index, period)) / (Math.pow(1 + index, period) - 1)));
         return payment;
     }
 }
